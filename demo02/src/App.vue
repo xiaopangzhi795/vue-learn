420bf0401f20ad371a07ca67a2af8276d235e992
rubik <rubixgeek795@gmail.com>
2022-05-24 23:01:23 +0800

    demo

---
+++ demo02/src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+
+import Table from "./components/Table.vue";
+const msg = "hello vue";
+
+</script>
+
+<template>
+  {{ msg }}
+
+  <Table />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ demo02/src/components/Table.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import {ref} from "vue";
+
+const header = ref([
+  {
+    name: "名字"
+  }
+]);
+
+const msg = "hello table";
+
+const isOption = false;
+
+const isShow = () => {
+  return !isOption;
+}
+
+</script>
+
+<template>
+  {{ msg }}
+  <table>
+    <thead>
+    <tr>
+      <th></th>
+      <th v-for="(item, index) in header" v-text="item.name"></th>
+      <th :class="{nShow: isShow}"><slot name="option"><button @click="console.log('click option button');">操作</button></slot></th>
+    </tr>
+    </thead>
+  </table>
+
+</template>
+
+<style scoped lang="less">
+@import "../assets/table.less";
+
+</style>
